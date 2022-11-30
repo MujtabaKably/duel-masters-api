87c77ed907fb83ca7078ea93fc6d45cf166bdae8
mozi <mozi@pop-os.localdomain>
2022-11-30 16:29:12 +0530

    feat: add basic filters to APIs

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { data } from '../final-processed-data';
-import { Card } from './card.model';
+
+type Filters = {
+  sets?: any[];
+  illustrators?: any[];
+  rarities?: any[];
+  civilization?: any[];
+  races?: any[];
+  types?: any[];
+  power?: string | number;
+  mana?: string | number;
+  properties?: any[];
+};
+
 @Injectable()
 export class CardsService {
-  getCards(): Card[] {
-    return data?.data;
+  getCards(skip = 0, limit = 15, filters: Filters = {}): any {
+    const totalData = data?.data;
+
+    return {
+      skip,
+      limit,
+      totalRecords: totalData.length,
+      data: totalData.slice(skip, skip + limit),
+    };
   }
 }
